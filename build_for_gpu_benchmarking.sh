94b9102744cb4f2152bbfd207a5cd2a82756569b
Lukas Rothenberger <lukas.rothenberger@tu-darmstadt.de>
2024-01-04 14:52:46 +0100

    fix: typo

--- build_for_gpu_benchmarking.sh
+++ build_for_gpu_benchmarking.sh
@@ -12,6 +12,6 @@ OMP_FLAGS="-fopenmp=libomp -fopenmp-targets=nvptx64 --libomptarget-nvptx-bc-path
 ## regular build + defined flags
 
 
-cmake -DCMAKE_C_COMPILER=${CC} -DCMAKE_CXX_COMPILER=${CXX} -DCMAKE_C_FLAGS="${CMAKE_C_FLAGS} ${OMP_FLAGS} -fopenmo" -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} ${OMP_FLAGS} -fopenmp" . 
+cmake -DCMAKE_C_COMPILER=${CC} -DCMAKE_CXX_COMPILER=${CXX} -DCMAKE_C_FLAGS="${CMAKE_C_FLAGS} ${OMP_FLAGS} -fopenmp" -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} ${OMP_FLAGS} -fopenmp" . 
 make
 
